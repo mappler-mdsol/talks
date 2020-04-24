bb754bfd85f6d4c66b2d402a04c5b92514451447
Harry Appler <mappler@mdsol.com>
2020-04-23 21:00:32 -0400

    initial setup

---
+++ rspec_talk/lib/example_class.rb
@@ -0,0 +1,37 @@
+# a class for demonstrating rspec
+class ExampleClass
+  def my_reverser(word_to_reverse)
+    reversed_string = ''
+
+    # Goes through the given word character by character
+    word_to_reverse.chars do |char|
+      # takes the current character, and adds it to the beginning of all the characters we've already reversed
+      new_reversed_string = char + reversed_string
+      # saves the newly created word in place of the old version
+      reversed_string = new_reversed_string
+    end
+    # returns once all the loops are done 
+    return reversed_string
+  end
+
+  def side_effect_fun(some_number_counter, number = 0)
+    some_number_counter.add_a_number(number)
+    some_number_counter.print_the_total
+    return 'it works!!!!!'
+  end
+end
+
+# a class that's an addition calculator
+class NumberCounter
+  def initialize( base_number = 0)
+    @total = base_number
+  end
+
+  def add_a_number(number = 0)
+    @total += number
+  end
+
+  def print_the_total
+    puts "the total is #{@total}"
+  end
+end
---
+++ rspec_talk/spec/example_class_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require 'example_class'
+
+describe 'Example' do
+  subject { ExampleClass.new }
+
+  describe '#my_reverser' do
+  end
+
+  describe '#side_effect_fun' do
+  end
+end
