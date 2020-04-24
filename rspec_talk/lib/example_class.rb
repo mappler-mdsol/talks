7b344c04c5ad0a1e12ab804a772558c5375fff3f
Harry Appler <mappler@mdsol.com>
2020-04-23 21:20:11 -0400

    with refactor and spec

--- rspec_talk/lib/example_class.rb
+++ rspec_talk/lib/example_class.rb
@@ -1,18 +1,7 @@
 # a class for demonstrating rspec
 class ExampleClass
   def my_reverser(word_to_reverse)
-    reversed_string = ''
-
-    # Goes through the given word character by character
-    word_to_reverse.chars do |char|
-      # takes the current character, adds it together with all the characters we've already reversed, 
-      # and saves that new word
-      new_reversed_string = char + reversed_string
-      # saves that new  word in place of the old version
-      reversed_string = new_reversed_string
-    end
-    # returns once all the loops are done 
-    return reversed_string
+   word_to_reverse.reverse
   end
 
   def side_effect_fun(some_number_counter, number = 0)
--- rspec_talk/spec/example_class_spec.rb
+++ rspec_talk/spec/example_class_spec.rb
@@ -5,6 +5,9 @@ describe 'Example' do
   subject { ExampleClass.new }
 
   describe '#my_reverser' do
+    it 'reverses the given word' do
+      expect(subject.my_reverser('FooBar')).to eq('raBooF')
+    end
   end
 
   describe '#side_effect_fun' do
